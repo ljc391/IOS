6966872cff25c8bcc4869efb85f4b592d1b920c7
ljc391 <ljc391@nyu.edu>
2015-03-02 16:12:36 -0500

    HW2
    HW2

---
+++ HW2/BlackJack2/card.swift
@@ -0,0 +1,15 @@
+//
+//  card.swift
+//  BlackJack2
+//
+//  Created by Elaine Chang on 2/26/15.
+//  Copyright (c) 2015 Elaine Chang. All rights reserved.
+//
+
+import Foundation
+
+class card{
+    var numofcard:Int32 = 0
+    var number = [Int]()
+    var suit = [Int]()
+}
