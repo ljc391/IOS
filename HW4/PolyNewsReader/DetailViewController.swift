2d9f6a6edbe75db73197e07f8ec389e05d603c72
ljc391 <ljc391@nyu.edu>
2015-04-14 20:24:20 -0400

    HW4

---
+++ HW4/PolyNewsReader/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  PolyNewsReader
+//
+//  Created by Ardis Kadiu on 4/7/15.
+//  Copyright (c) 2015 Spark451. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    var detailItem:AnyObject?
+    var titleItem:AnyObject?
+    var imageItem:AnyObject?
+    
+    @IBOutlet weak var LabelTitle: UILabel!
+    @IBOutlet weak var ImageP: UIImageView!
+    @IBOutlet weak var LabelDetail: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewDidAppear(animated: Bool) {
+        if let detail:AnyObject = self.detailItem{
+            LabelDetail.text = detail.description
+            LabelTitle.text = titleItem?.description
+            
+            if  let data = NSData(contentsOfURL: imageItem! as NSURL) {
+                ImageP.image = UIImage(data: data)
+            }
+
+        }// Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
